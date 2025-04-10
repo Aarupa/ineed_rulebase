56b172cc02ec9f6972fe97ce342503fc7be29378
Akanksha Shivale <akankshashivale21@gmail.com>
2025-04-10 11:06:27 +0530

    Changes in database-uri

--- chat_frontend/train.py
+++ chat_frontend/train.py
@@ -4,7 +4,7 @@ import os
 
 script_dir = os.path.dirname(__file__)  # Directory of the current script
 chatbot = ChatBot("MyBot", storage_adapter="chatterbot.storage.SQLStorageAdapter",
-    database_uri="sqlite:///C:/Desktop/chatbout_/ineed_rulebase/chat_frontend/db.sqlite3")
+    database_uri="sqlite:///db.sqlite3")
 trainer = ChatterBotCorpusTrainer(chatbot)
 
 #Train chatbot with explicit corpus file paths
